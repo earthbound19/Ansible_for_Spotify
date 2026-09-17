170c8c961cf824baf939cb328bd5b314f4e2fef1
Alex Hall <earthbound19@users.noreply.github.com>
2026-09-16 20:58:03 -0600

    simple .bat script to launch

---
+++ Launch_Ansible_for_Spotify.bat
@@ -0,0 +1,4 @@
+%SystemRoot%\System32\cmd.exe /k "Python Ansible_for_Spotify.py"
+
+:: a windows terminal (new modern app with tabbed terminals) that ran the above commands; the profile no longer exists; this command will open a new terminal by profile name though! :
+:: wt new-tab --profile "Ansible for Spotify"
